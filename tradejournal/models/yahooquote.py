f1f302c5fd1a5a0c1a5921cf3925efe5ba7375a4
Md Tanveer Alam <mdalam@microsoft.com>
2019-12-18 10:31:20 +0530

    Switch to BSE for realtime quotes

--- tradejournal/models/yahooquote.py
+++ tradejournal/models/yahooquote.py
@@ -5,7 +5,7 @@ import datetime
 import os
  
 def get_yahoo_quote(symbol='SBIN', data_range='90d', data_interval='1h'):
-    res = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/{symbol}.NS?range={data_range}&interval={data_interval}'.format(**locals()))
+    res = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/{symbol}.BO?range={data_range}&interval={data_interval}'.format(**locals()))
     data = res.json()
     body = data['chart']['result'][0]    
     dt = datetime.datetime
