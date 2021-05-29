889f73fef62f9998a12f26dedc45fd5631a86a36
Tanveer <md.tanveer@outlook.com>
2021-05-29 16:16:00 +0530

    Working charts

--- tradejournal/static/scripts/tradejournalchart.js
+++ tradejournal/static/scripts/tradejournalchart.js
@@ -9,7 +9,7 @@ class TJChart
         this.primaryIndicator = primaryIndicator;
         this.currentChartIndex = 0;
 
-        var dataWindowSize = 250;
+        this.dataWindowSize = 250;
         var windowW = Math.round(window.innerWidth*0.81);
         var windowH = Math.round(window.innerHeight*0.70);
         var currentScrip = "";
@@ -35,60 +35,60 @@ class TJChart
         var indicatorTop = d3.scaleLinear()
                 .range([dim.indicator.top, dim.indicator.bottom]);
 
-        var x = techan.scale.financetime()
+        this.x = techan.scale.financetime()
                 .range([0, dim.plot.width]);
 
-        var y = d3.scaleLinear()
+        this.y = d3.scaleLinear()
                 .range([dim.ohlc.height, 0]);
 
-        var yVolume = d3.scaleLinear()
-                .range([y(0), y(0.2)]);
+        this.yVolume = d3.scaleLinear()
+                .range([this.y(0), this.y(0.2)]);
 
-        var plotHeikenAshi = false;
+        this.plotHeikenAshi = false;
 
-        if (!plotHeikenAshi)
+        if (!this.plotHeikenAshi)
         {
-            var candlestick = techan.plot.candlestick()
-                    .xScale(x)
-                    .yScale(y);
+            this.candlestick = techan.plot.candlestick()
+                    .xScale(this.x)
+                    .yScale(this.y);
         }
         else
         {
-            var candlestick = techan.plot.heikinashi()
-                    .xScale(x)
-                    .yScale(y);
+            this.candlestick = techan.plot.heikinashi()
+                    .xScale(this.x)
+                    .yScale(this.y);
 
-            var heikinashiIndicator = techan.indicator.heikinashi();
+            this.heikinashiIndicator = techan.indicator.heikinashi();
         }
 
 
         if (this.primaryIndicator == "ichimoku") {
-            var ichimoku = techan.plot.ichimoku()
-                .xScale(x)
-                .yScale(y);
+            this.ichimoku = techan.plot.ichimoku()
+                .xScale(this.x)
+                .yScale(this.y);
         }
         else {
-            var bollinger = techan.plot.bollinger()
-                .xScale(x)
-                .yScale(y);
+            this.bollinger = techan.plot.bollinger()
+                .xScale(this.x)
+                .yScale(this.y);
         }
 
-        var xAxis = d3.axisBottom(x);
+        this.xAxis = d3.axisBottom(this.x);
 
-        var yAxis = d3.axisLeft(y)
+        this.yAxis = d3.axisLeft(this.y)
                 .tickFormat(d3.format(",.3s"));
 
-        var volumeAxis = d3.axisRight(yVolume)
+        var volumeAxis = d3.axisRight(this.yVolume)
                 .ticks(3)
                 .tickFormat(d3.format(",.3s"));
 
-        var svg = d3.select("#chart").append("svg")
+        this.svg = d3.select("#chart").append("svg")
                 .attr("width", windowW)
                 .attr("height", windowH)
             .append("g")
                 .attr("transform", "translate(" + dim.margin.left + "," + dim.margin.top + ")");
 
-        var defs = svg.append("defs");
+        var defs = this.svg.append("defs");
 
         defs.append("clipPath")
                 .attr("id", "ohlcClip")
@@ -109,78 +109,78 @@ class TJChart
                 .attr("height", dim.indicator.height);
 
 
-        var indicatorScale = d3.scaleLinear()
+        this.indicatorScale = d3.scaleLinear()
                 .range([indicatorTop(0)+dim.indicator.height, indicatorTop(0)]);
 
                 if (this.secondaryIndicator == "stochastic") {
-                    var stochastic = techan.plot.stochastic()
-                        .xScale(x)
-                        .yScale(indicatorScale);
+                    this.stochastic = techan.plot.stochastic()
+                        .xScale(this.x)
+                        .yScale(this.indicatorScale);
 
-                    var stochasticIndicator = techan.indicator.stochastic()
+                    this.stochasticIndicator = techan.indicator.stochastic()
                 }
                 else {
-                    var macd = techan.plot.macd()
-                        .xScale(x)
-                        .yScale(indicatorScale);
+                    this.macd = techan.plot.macd()
+                        .xScale(this.x)
+                        .yScale(this.indicatorScale);
 
-                    var macdIndicator = techan.indicator.macd()
+                    this.macdIndicator = techan.indicator.macd()
                 }
-        var indicatorAxisLeft = d3.axisLeft(indicatorScale)
+        this.indicatorAxisLeft = d3.axisLeft(this.indicatorScale)
                 .ticks(3);
 
         var indicatorAnnotationLeft = techan.plot.axisannotation()
-                .axis(indicatorAxisLeft)
+                .axis(this.indicatorAxisLeft)
                 .orient("left")
                 .format(d3.format(',.2f'));
 
-        var indicatorSelection = svg.selectAll("svg > g.indicator").data(["indicator"]).enter()
+        var indicatorSelection = this.svg.selectAll("svg > g.indicator").data(["indicator"]).enter()
                  .append("g")
                  .attr("class", function (d) { return d; });
 
         indicatorSelection.append("g")
                 .attr("class", "axis right")
-                .attr("transform", "translate(" + x(1) + ",0)");
+                .attr("transform", "translate(" + this.x(1) + ",0)");
 
         indicatorSelection.append("g")
                 .attr("class", "axis left")
-                .attr("transform", "translate(" + x(0) + ",0)");
+                .attr("transform", "translate(" + this.x(0) + ",0)");
 
         indicatorSelection.append("g")
                 .attr("class", "indicator-plot")
                 .attr("clip-path", function(d, i) { return "url(#indicatorClip-" + i + ")"; });
 
         if (this.primaryIndicator == "ichimoku") {
-            var ichimokuIndicator = techan.indicator.ichimoku();
+            this.ichimokuIndicator = techan.indicator.ichimoku();
             // Don't show where indicators don't have data
-            var indicatorPreRoll = ichimokuIndicator.kijunSen() + ichimokuIndicator.senkouSpanB();
+            this.indicatorPreRoll = this.ichimokuIndicator.kijunSen() + this.ichimokuIndicator.senkouSpanB();
         } else {
-            var bollingerIndicator = techan.indicator.bollinger();
+            this.bollingerIndicator = techan.indicator.bollinger();
         }
 
-        var volume = techan.plot.volume()
-                .accessor(candlestick.accessor())   // Set the accessor to a ohlc accessor so we get highlighted bars
-                .xScale(x)
-                .yScale(yVolume);
+        this.volume = techan.plot.volume()
+                .accessor(this.candlestick.accessor())   // Set the accessor to a ohlc accessor so we get highlighted bars
+                .xScale(this.x)
+                .yScale(this.yVolume);
 
-        var tradearrow = techan.plot.tradearrow()
-            .xScale(x)
-            .yScale(y)
+        this.tradearrow = techan.plot.tradearrow()
+            .xScale(this.x)
+            .yScale(this.y)
             .orient(function(d) { return d.type.startsWith("buy") ? "up" : "down"; })
 
-        svg.append("g")
+        this.svg.append("g")
             .attr("class", this.primaryIndicator)
             .attr("clip-path", "url(#ohlcClip)");
 
-        svg.append("g")
+        this.svg.append("g")
                 .attr("class", "candlestick")
                 .attr("clip-path", "url(#ohlcClip)");
 
-        svg.append("g")
+        this.svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + dim.indicator.bottom + ")");
 
-        svg.append("g")
+        this.svg.append("g")
                 .attr("class", "y axis")
             .append("text")
                 .attr('id', 'charttitle')
@@ -189,70 +189,70 @@ class TJChart
                 .attr("dy", ".71em")
                 .style("text-anchor", "end")
                 .text(symbol + '(' + timeframe + ')');
-        svg.append("g")
+        this.svg.append("g")
                 .attr("class", "volume")
                 .attr("clip-path", "url(#ohlcClip)");
 
-        svg.append("g")
+        this.svg.append("g")
                   .attr("class", "tradearrow")
                     .attr("clip-path", "url(#ohlcClip)");
     }
 
     draw(data) {
-        if (plotHeikenAshi)
+        if (this.plotHeikenAshi)
         {
-            candlestickData = heikinashiIndicator(data);
+            var candlestickData = this.heikinashiIndicator(data);
         }
         else
         {
-            candlestickData = data;
+            var candlestickData = data;
         }
 
         if (this.primaryIndicator == "ichimoku") {
-            var ichimokuData = ichimokuIndicator(data);
-            x.domain(data.map(ichimokuIndicator.accessor().d));
+            var ichimokuData = this.ichimokuIndicator(data);
+            this.x.domain(data.map(this.ichimokuIndicator.accessor().d));
             // Calculate the y domain for visible data points (ensure to include Kijun Sen additional data offset)
-            var y_domain = techan.scale.plot.ichimoku(ichimokuData.slice(indicatorPreRoll - ichimokuIndicator.kijunSen())).domain();
+            var y_domain = techan.scale.plot.ichimoku(ichimokuData.slice(this.indicatorPreRoll - this.ichimokuIndicator.kijunSen())).domain();
             y_domain = [y_domain[0] * 0.95, y_domain[1] * 1.05];
-            y.domain(y_domain);
-            yVolume.domain(techan.scale.plot.volume(data.slice(indicatorPreRoll - ichimokuIndicator.kijunSen())).domain());
+            this.y.domain(y_domain);
+            this.yVolume.domain(techan.scale.plot.volume(data.slice(this.indicatorPreRoll - this.ichimokuIndicator.kijunSen())).domain());
 
             // Logic to ensure that at least +KijunSen displacement is applied to display cloud plotted ahead of ohlc
-            x.zoomable().clamp(false).domain([indicatorPreRoll, data.length + ichimokuIndicator.kijunSen()]);
-            svg.selectAll("g.ichimoku").datum(ichimokuData).call(ichimoku);
+            this.x.zoomable().clamp(false).domain([this.indicatorPreRoll, data.length + this.ichimokuIndicator.kijunSen()]);
+            this.svg.selectAll("g.ichimoku").datum(ichimokuData).call(this.ichimoku);
         } else {
-            var bollingerData = bollingerIndicator(data);
-            x.domain(bollingerData.map(bollingerIndicator.accessor().d));
-            y.domain(techan.scale.plot.bollinger(bollingerData).domain());
-            yVolume.domain(techan.scale.plot.volume(data).domain());
-            svg.selectAll("g.bollinger").datum(bollingerData).call(bollinger);
+            var bollingerData = this.bollingerIndicator(data);
+            this.x.domain(bollingerData.map(this.bollingerIndicator.accessor().d));
+            this.y.domain(techan.scale.plot.bollinger(bollingerData).domain());
+            this.yVolume.domain(techan.scale.plot.volume(data).domain());
+            this.svg.selectAll("g.bollinger").datum(bollingerData).call(this.bollinger);
 
         }
-        svg.selectAll("g.candlestick").datum(candlestickData).call(candlestick);
-        svg.selectAll("g.x.axis").call(xAxis);
-        svg.selectAll("g.y.axis").call(yAxis);
-        svg.selectAll("g.volume").datum(data).call(volume);
+        this.svg.selectAll("g.candlestick").datum(candlestickData).call(this.candlestick);
+        this.svg.selectAll("g.x.axis").call(this.xAxis);
+        this.svg.selectAll("g.y.axis").call(this.yAxis);
+        this.svg.selectAll("g.volume").datum(data).call(this.volume);
 
         if (this.secondaryIndicator == "stochastic") {
-            var stochasticData = stochasticIndicator(data);
-            indicatorScale.domain(techan.scale.plot.stochastic(stochasticData).domain());
-            svg.selectAll("g.indicator .indicator-plot").datum(stochasticData).call(stochastic);
+            var stochasticData = this.stochasticIndicator(data);
+            this.indicatorScale.domain(techan.scale.plot.stochastic(stochasticData).domain());
+            this.svg.selectAll("g.indicator .indicator-plot").datum(stochasticData).call(this.stochastic);
         }
         else {
-            var macdData = macdIndicator(data);
-            indicatorScale.domain(techan.scale.plot.macd(macdData).domain());
-            svg.selectAll("g.indicator .indicator-plot").datum(macdData).call(macd);
+            var macdData = this.macdIndicator(data);
+            this.indicatorScale.domain(techan.scale.plot.macd(macdData).domain());
+            this.svg.selectAll("g.indicator .indicator-plot").datum(macdData).call(this.macd);
         }
-        svg.selectAll("g.indicator .axis.left").call(indicatorAxisLeft);
+        this.svg.selectAll("g.indicator .axis.left").call(this.indicatorAxisLeft);
 
         if (trades.length == 2 && trades[1].date > data[data.length - 1].date) {
-            chartTrades = [...trades].splice(0, 1);
+            var chartTrades = [...trades].splice(0, 1);
         }
         else {
-            chartTrades = trades;
+            var chartTrades = trades;
         }
-        svg.selectAll("g.tradearrow").selectAll("*").remove();
-        svg.selectAll("g.tradearrow").datum(chartTrades).call(tradearrow);
+        this.svg.selectAll("g.tradearrow").selectAll("*").remove();
+        this.svg.selectAll("g.tradearrow").datum(chartTrades).call(this.tradearrow);
     }
 
 
@@ -265,8 +265,9 @@ class TJChart
 
             document.getElementById('title').innerText = this.charts[i].title + " (" + (i+1) + " of " + this.charts.length + ")";
             document.getElementById('main').hidden = false;
+            var selfInstance = this;
             function D3_DataCallback(error, data) {
-                this.charts[i].raw_data = data;
+                selfInstance.charts[i].raw_data = data;
                 data = data.map(function (d) {
                     var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
                     // Open, high, low, close generally not required, is being used here to demonstrate colored volume
@@ -280,19 +281,19 @@ class TJChart
                         close: +d.close
                     };
                 })
-                if (dataWindowSize < data.length) {
-                    data = data.slice(data.length - dataWindowSize, data.length)
+                if (selfInstance.dataWindowSize < data.length) {
+                    data = data.slice(data.length - selfInstance.dataWindowSize, data.length)
                 }
-                draw(data);
+                selfInstance.draw(data);
             }
             if (this.charts[i].raw_data == null) {
                 var temp = this.charts[i].relativeUrl.split('?');
-                params = new URLSearchParams(temp[1]);
+                var params = new URLSearchParams(temp[1]);
                 params.set('tf', this.timeFrame);
-                if (resampleType) {
-                    params.set('type', resampleType);
+                if (this.resampleType) {
+                    params.set('type', this.resampleType);
                 }
-                relativeUrl = temp[0] + '?' + params;
+                var relativeUrl = temp[0] + '?' + params;
                 d3.csv(relativeUrl, D3_DataCallback)
             } else {
                 D3_DataCallback(null, this.charts[i].raw_data);
@@ -301,7 +302,7 @@ class TJChart
 
         RenderCurrentChart() {
             this.charts[this.currentChartIndex].raw_data = null;
-            RenderChart(this.currentChartIndex, this.timeFrame, resampleType);
+            this.RenderChart();
         }
 
         RenderNextChart() {
@@ -309,7 +310,7 @@ class TJChart
             if (this.currentChartIndex >= this.charts.length)
                 this.currentChartIndex = 0;
             if (this.charts.length > 0)
-                RenderChart(this.currentChartIndex, this.timeFrame, this.resampleType);
+               this.RenderChart();
         }
 
         RenderPreviousChart() {
@@ -317,14 +318,14 @@ class TJChart
             if (this.currentChartIndex < 0)
                 this.currentChartIndex = this.charts.length-1;
             if (this.charts.length > 0)
-                RenderChart(this.currentChartIndex, this.timeFrame, this.resampleType);
+                this.RenderChart();
             }
 
         ToggleIndicator() {
-            oind = this.primaryIndicator == "bollinger" ? "ichimoku" : "bollinger";
-            params = new URLSearchParams(window.location.search);
+            var oind = this.primaryIndicator == "bollinger" ? "ichimoku" : "bollinger";
+            var params = new URLSearchParams(window.location.search);
             params.set('oind', oind);
-            newUrl = window.location.origin + window.location.pathname + '?' + params;
+            var newUrl = window.location.origin + window.location.pathname + '?' + params;
             window.location.href = newUrl;
         }
 
